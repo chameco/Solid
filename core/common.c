381d5fff3b40720f0d75049374bc4ed8d73baf42
CharmlessCoin <charmlesscoin@outlook.com>
2013-10-16 22:57:01 -0400

    Updated report_error() to take formatted strings.

--- core/common.c
+++ core/common.c
@@ -1,34 +1,39 @@
 #include "common.h"
 #include <string.h>
+#include <stdarg.h>
 #include <stdlib.h>
 
-/* Print out a general error message in *string. Use perror if
- *	errno is set, otherwise just print out *string plus a prefix. */
-void report_error(char *string)
+/* Print the formatted string from applying "..."
+ *  to "fmt" and call perror if errno is non-zero. */
+void report_error(char *fmt, ...)
 {
-	char *prefix = "Solid";
-	char *error = NULL;
-	int len = strlen(prefix) + strlen(string);
+    char *prefix = "Solid";
+    char *error = NULL;
+    int len = 0;
+    va_list args;
 
-	error = calloc(1, len + 1);
-	if(error == NULL) {
-		error = prefix;
-	} else {
-		sprintf(error, "%s: %s", prefix, string);
-	}
+    va_start(args, fmt);
 
-	if(errno) {
-		perror(error);
-	} else {
-		fputs(error, stderr);
-	}
+    /* snprintf() returns the length of the
+     *  string if the first argument is NULL. */
+    len = vsnprintf(NULL, 0, fmt, args);
 
-	free(error);
+    error = calloc(1, len + 1);
+    vsnprintf(error, len + 1, fmt, args);
+
+    if(errno) {
+        fprintf(stderr, "%s: ", prefix);
+        perror(error);
+    } else {
+        fprintf(stderr, "%s: %s\n", prefix, error);
+    }
+    va_end(args);
+    free(error);
 }
 
 int yyerror(struct YYLTYPE *yylloc_param, void *scanner, struct ast_node **root, const char *s)
 {
-	log_err("%s at line %d", s, yylloc_param->first_line);
-	exit(1);
-	return 0;
+    log_err("%s at line %d", s, yylloc_param->first_line);
+    exit(1);
+    return 0;
 }
